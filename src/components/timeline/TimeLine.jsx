d4f8107aaa6558a1303d9c4424d7e8ef85a9d49a
kandalog <asuasu.tee@gmail.com>
2022-08-29 21:53:56 +0900

    Homeを作成

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Post } from "../post/Post";
+import { Share } from "../share/Share";
 
 import "./TimeLine.css";
 
 export const TimeLine = () => {
   return (
     <div className="timeline">
-      <div className="timelineWrapper">{/* <Share /> */}</div>
+      <div className="timelineWrapper">
+        <Share />
+        <Post />
+      </div>
     </div>
   );
 };
